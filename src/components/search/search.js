29da4aaff9dc09d61d9b7199b6ac3bce0420ec04
Emre KARAMAN <matizdrum@gmail.com>
2022-09-17 21:52:15 +0300

    update

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,33 +1,34 @@
-
 import { useState } from "react"
 import { AsyncPaginate } from "react-select-async-paginate"
 import { GEO_API_URL, geoApiOptions } from "../../api"
 
+
+
 const Search = ({onSearchChange}) => {
     
-    const [search ,setSearch] =useState(null)
+    const [search, setSearch] = useState(null)
 
     const loadOptions = (inputValue) => {
-        return fetch(`${GEO_API_URL}/cities?minPopulation=100000namePrefix=${inputValue}`, geoApiOptions)
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,geoApiOptions)
         .then((response) => response.json())
         .then((response) => {
             return {
                 options: response.data.map((city) => {
-                    return{
+                    return {
                         value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name} ${city.countryCode}`
+                        label: `${city.name} ${city.countryCode}`,
                     }
                 })
             }
         })
-        .catch((err) => console.log(err))
+        .catch((err) => console.error(err));
     }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData)
         onSearchChange(searchData)
     }
-
+    
     return(
         <AsyncPaginate 
         placeholder="Search for city"
