f4fc70c454b7bae456cb90342ca5ba23e72ffa79
Emre KARAMAN <matizdrum@gmail.com>
2022-09-17 22:06:48 +0300

    update

--- src/components/current-weather/current-weater.js
+++ src/components/current-weather/current-weater.js
@@ -9,7 +9,7 @@ const CurrentWeather = ({ data }) => {
                     <p className='weather-description'>{data.weather[0].description}</p>
                 </div>
 
-                <img alt="weather" className='weather-icon' src={`icons/${data.weather[0].icon}.png`} />
+                <img alt="weather" className='weather-icon' src={`public/icons/${data.weather[0].icon}.png`} />
 
             </div>
             <div className='bottom'>
