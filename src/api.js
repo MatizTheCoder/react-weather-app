b3fbff1c4ece7e360c330f991368501126967273
Emre KARAMAN <matizdrum@gmail.com>
2022-09-17 21:09:46 +0300

    api done

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '88bd2a2317msh1884f8c1e079ebbp11d344jsn0fa27a0cdb4d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY="ede18978f199b083fdab4d8901df6d63";
