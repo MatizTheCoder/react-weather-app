68c02299ff13f849c6ed7999df41bd6518386a17
MatizTheCoder <107365670+MatizTheCoder@users.noreply.github.com>
2022-09-18 10:39:07 +0300

    Update api.js
    comment lien

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
+//Use your own account settings in this area
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '88bd2a2317msh1884f8c1e079ebbp11d344jsn0fa27a0cdb4d',
+		'X-RapidAPI-Key': '***',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,4 +12,4 @@ export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY="ede18978f199b083fdab4d8901df6d63";
+export const WEATHER_API_KEY="***";
